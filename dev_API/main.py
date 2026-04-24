f291f0663ad3db3fa158bbf1fb0d2b47a08bf29f
YassineSdk <yassine.sadiki2-etu@etu.univh2c.ma>
2026-04-24 15:35:28 +0100

    refactor: enhance API key security and logging setup

--- dev_API/main.py
+++ dev_API/main.py
@@ -1,46 +1,48 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , Security,HTTPException, status
+from fastapi.security import APIKeyHeader
+from contextlib import asynccontextmanager
 from pydantic import BaseModel 
-import logging
 import logfire 
-from dotenv import load_dotenv
+from dotenv import load_dotenv,find_dotenv
 import os
 from datetime import datetime
 
-load_dotenv("../.env")
+load_dotenv(find_dotenv())
 
-# setup a logger 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # setup the logfire
-logfire.configure(token=os.getenv("logfire_key"))
+logfire.configure(token=os.getenv("logfire_key"),
+                console=logfire.ConsoleOptions(verbose=True))
+# getting the key fro .env
+API_KEY = os.getenv("API_key")
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=True)
 
-
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logfire.info("Knowledge API started", date=str(datetime.today()))
+    yield
+    logfire.info("Knowledge API closed", date=str(datetime.today()))
 
 # defining the app object to create an instance of FastAPi framework 
 app = FastAPI(title='Knowledge API',
             description="""this Service is responsable for generating a referencial of 
-            knowledge related to a mission topic in the context of internal audit"""
+            knowledge related to a mission topic in the context of internal audit""",
+            lifespan=lifespan,
             )
+
 # autolog all the evry request, body
 logfire.instrument_fastapi(app)
-@app.on_event("startup")
-async def on_startup():
-    logger.info("=====================================")
-    logger.info("Knowledge APi is starting up....")
-    logger.info(f"Date : {datetime.today()}")
-    logger.info("=====================================")
-    logfire.info("Knowledge API started ", date=str(datetime.today()))
 
-@app.on_event("shutdown")
-async def on_shutdown():
-    logger.info("=====================================")
-    logger.info("Knowledge API is shutting down....")
-    logger.info("=====================================")
-    logfire.info("Knowledge API closed ", date=str(datetime.today()))
+# acess verification using authentification system API_key
+def verify_access_permission(api_key: str =Security(api_key_header)):
+    if api_key != API_KEY :
+        logfire.info("access denied, invalid API key")
+        raise HTTPException(status_code=403, detail='invalid API Key')
+    return api_key
+
 
 # defining the shape of the request body using pydantic Basemodel
-# Pydantic automaticly validates the recieved data against the schema if missing or wrong type the FastAPI retuns an error 422
+# Pydantic automaticely validates the recieved data against the schema if missing or wrong type the FastAPI retuns an error 422
 class MissionTopic(BaseModel):
     mission : str 
 
@@ -50,9 +52,9 @@ def root():
 
 
 @app.post('/Knowledge_API')
-def Knowledge_collection(mission=MissionTopic):
-
-    logging.info(f"Received mission: {mission}")
+def Knowledge_collection(mission: MissionTopic,api_key: str =Security(api_key_header)):
+    verify_access_permission(api_key)
+    logfire.info("access accepted")
     logfire.info(f"Received mission: {mission}")
-    return {"mission_topic":mission}
+    return {"mission_topic:": mission.mission}
 
